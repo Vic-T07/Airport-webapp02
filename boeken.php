60ce5fdc3020b6f6d636309eaf5469067b805437
ElcoTerwoert <elco.terwoert@gmail.com>
2024-06-22 16:34:02 +0200

    alle code die niet nodig was verwijderd

--- boeken.php
+++ boeken.php
@@ -11,26 +11,6 @@ include ('header.php');  ?></header>
 <main id="ticketpagina" style="background-image: url('assets/img/image8.png'); background-size: auto;">
 
 <body>
-    <!-- <div class="ticket">
-            <?php
-            include("gettravels.php");
-                if(!empty($result)){
-
-                    $first_travel = $result[6];
-
-                    echo '<img src=' . $first_travel['image_url'] . '>';
-                    echo '<h2>' . $first_travel['to_city'],", ",  $first_travel['to_country']. '</h2>';
-                    echo '<p>' . "Departure: TBA" . '<p>';
-                    echo '<p>' . $first_travel['date'] . '</p>';
-                    echo '<p>' . $first_travel['travel_time'] . '</p>';
-                    echo '<p>' . "Retour ticket" . '<p>';
-                    echo '<h2>' ."vanaf €", $first_travel['cost'] . '</h2>';
-                }
-                else{
-                    echo 'No Travel';
-                }
-    ?>
-    </div> -->
 <div class="tickets-container">
     <?php
     include("gettravels.php");
@@ -53,16 +33,6 @@ include ('header.php');  ?></header>
         echo 'No Travel';
     }
     ?>
-<!-- </div>
-    <div class="ticket">
-        <img src="assets/img/image9.png" alt="Verona">
-        <h2>Verona, Italië</h2>
-        <p>Vertrek: 10:00</p>
-        <p>17-05-2024</p>
-        <p>1 u 40 min</p>
-        <p>retour ticket</p>
-        <p class="price">vanaf €300</p>
-    </div> -->
 </body>
 
 </main>
